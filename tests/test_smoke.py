9e02b5d4bd1bd4e7b427d8f7c8960a2f63db751e
creatos1 <www.gamercracks@gmail.com>
2025-09-28 15:13:51 -0600

    Proyecto

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,6 +1,4 @@
-# tests/test_smoke.py
 def test_import_main():
-    """Verifica que la aplicación principal se pueda importar sin errores."""
     from src.main import app
 
     assert app is not None
